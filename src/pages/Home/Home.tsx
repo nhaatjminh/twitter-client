dabaf791998f2989529e6cae29bd8e733d864fd4
nhaatjminh <minhphan.240199@gmail.com>
2023-11-21 00:40:20 +0700

    fetching video hls

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
 import { getGoogleAuth } from "../../utils/googleAuth";
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
 
 const url = getGoogleAuth();
 
@@ -24,8 +31,19 @@ export default function Home() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>HLS Streaming</h2>
+      <MediaPlayer
+        title="Sprite Fight"
+        src="http://localhost:5000/static/video-hls/3afbe6ce-9eb6-4571-a058-485a8a9f89b5/master.m3u8"
+      >
+        <MediaProvider />
+        <DefaultVideoLayout
+          thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt"
+          icons={defaultLayoutIcons}
+        />
+      </MediaPlayer>
       <video controls width={600}>
-        <source 
+        <source
           src="http://localhost:5000/static/video-stream/6d6928106463d7a227383ef00.mp4"
           type="video/mp4"
         />
@@ -33,8 +51,8 @@ export default function Home() {
       <p className="read-the-docs">
         {isAuthen ? (
           <>
-          <span>Hello my friend, you are logged in.</span>
-          <button onClick={logout}>Logout</button>
+            <span>Hello my friend, you are logged in.</span>
+            <button onClick={logout}>Logout</button>
           </>
         ) : (
           <Link to={url}>Login with google</Link>
